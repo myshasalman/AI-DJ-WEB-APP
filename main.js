bc207d07e8874f9adfe306c3e6d8e8de6c90a77a
myshasalman <77440425+myshasalman@users.noreply.github.com>
2021-11-29 15:45:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+song="";
+function preload()
+{
+    song= loadSound("music.mp3");
+}
+function setup()
+{
+    canvas= createCanvas(600,400);
+    canvas.center();
+     video= createCapture(VIDEO);
+     video.hide();
+}
+function draw()
+{
+    image(video,0,0,600,400);
+}
+function playsound()
+{
+    song.play();
+}
