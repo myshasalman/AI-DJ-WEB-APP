90281ab39141ba0526933d08d2ffc46c169943aa
myshasalman <77440425+myshasalman@users.noreply.github.com>
2021-12-17 15:54:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftwristx=0;
+leftwristy=0;
+rightwristx=0;
+rightwristy=0;
 function preload()
 {
     song= loadSound("music.mp3");
@@ -9,6 +13,8 @@ function setup()
     canvas.center();
      video= createCapture(VIDEO);
      video.hide();
+     poseNet=ml5.poseNet(video, modelLoaded);
+     poseNet.on('pose', gotPoses);
 }
 function draw()
 {
@@ -17,4 +23,25 @@ function draw()
 function playsound()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function modelLoaded()
+{
+    console.log("posenet is Initalized");
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        rightwristx=results[0].pose.rightWrist.x;
+        rightwristy=results[0].pose.rightWrist.y;
+        console.log("RightWristx= "+rightwristx+"RightWristy ="+rightwristy);
+
+        leftwristx=results[0].pose.leftWrist.x;
+        leftwristy=results[0].pose.leftWrist.y;
+        console.log("LeftWristx= "+leftwristx+"LeftWristy ="+leftwristy);
+    }
 }
