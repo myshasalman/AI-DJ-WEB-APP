aaf96e3199ef1988d7e359989c325bef9d896908
myshasalman <77440425+myshasalman@users.noreply.github.com>
2021-12-22 15:49:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 song="";
+scorerightwrist =0;
 scoreleftwrist =0;
 leftwristx=0;
 leftwristy=0;
@@ -10,7 +11,7 @@ function preload()
 }
 function setup()
 {
-    canvas= createCanvas(600,400);
+    canvas= createCanvas(600,500);
     canvas.center();
      video= createCapture(VIDEO);
      video.hide();
@@ -19,7 +20,7 @@ function setup()
 }
 function draw()
 {
-    image(video,0,0,600,400);
+    image(video,0,0,600,500);
     fill("#0c083b");
     stroke("#0c083b");
     if (scoreleftwrist>0.2)
@@ -27,9 +28,38 @@ function draw()
         circle(leftwristx, leftwristy, 20);
         innumberleftwristy=Number(leftwristy);
         remove_decimal= floor(innumberleftwristy);
-        volume = remove_decimal/400;
+        volume = remove_decimal/500;
         document.getElementById("volume").innerHTML ="volume = " + volume;
         song.setVolume(volume);
+    }
+    if(scorerightwrist> 0.2)
+    {
+        circle(rightwristx,rightwristy, 20);
+        if(rightwristy>0 && rightwristy<=100)
+        {
+            document.getElementById("speed").innerHTML="speed = 0.5x";
+            song.rate(0.5);
+        }
+        else if(rightwristy>100 && rightwristy<=200)
+        {
+            document.getElementById("speed").innerHTML="speed = 1.0x";
+            song.rate(1.0);
+        }
+        else if(rightwristy>200 && rightwristy<=300)
+        {
+            document.getElementById("speed").innerHTML="speed = 1.5x";
+            song.rate(1.5);
+        }
+        else if(rightwristy>300 && rightwristy<=400)
+        {
+            document.getElementById("speed").innerHTML="speed = 2.0x";
+            song.rate(2.0);
+        }
+        else if(rightwristy>400)
+        {
+            document.getElementById("speed").innerHTML="speed = 2.5x";
+            song.rate(2.5);
+        }
     }
 }
 function playsound()
@@ -48,7 +78,8 @@ function gotPoses(results)
     {
         console.log(results);
         scoreleftwrist= results[0].pose.keypoints[9].score;
-
+        scorerightwrist= results[0].pose.keypoints[10].score;
+        console.log("Score LeftWrist= "+scoreleftwrist+ " Score RightWrist= "+scorerightwrist);
         rightwristx=results[0].pose.rightWrist.x;
         rightwristy=results[0].pose.rightWrist.y;
         console.log("RightWristx= "+rightwristx+"RightWristy ="+rightwristy);
